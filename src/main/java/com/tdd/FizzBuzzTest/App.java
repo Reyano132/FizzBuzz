9e8e09ed35b065a1f977876b8e28d5d38a8decec
Rishi <rushichaudhari26@gmail.com>
2020-11-18 17:45:42 +0530

    filter method changed to convert numbers divisible by 3 and 5 to FizzBuzz

--- src/main/java/com/tdd/FizzBuzzTest/App.java
+++ src/main/java/com/tdd/FizzBuzzTest/App.java
@@ -10,7 +10,7 @@ public class App
 	public String[] filter(int start,int end){
 		String[] result=new String[end-start+1];
 		for(int i=start,k=0;i<end+1;i++,k++) {
-			result[k]= i%3==0 ? "Fizz" : ( i%5==0 ? "Buzz" : String.valueOf(i));
+			result[k]= (i%3==0 && i%5==0) ? "FizzBuzz" : i%3==0 ? "Fizz" : ( i%5==0 ? "Buzz" : String.valueOf(i));
 		}
 		return result;
 	}
