c4e30ae53e401171ddcade815cb51aab3b90c296
Rishi <rushichaudhari26@gmail.com>
2020-11-18 16:59:33 +0530

    Created project with maven

---
+++ src/main/java/com/tdd/FizzBuzzTest/App.java
@@ -0,0 +1,13 @@
+package com.tdd.FizzBuzzTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
